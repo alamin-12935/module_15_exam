896ebc36e52a01e62cd0f02709db09b52fc18d19
larning <alaminkhan133742@gmail.com>
2026-04-10 12:14:28 +0600

    3st commit

--- lib/Travel.dart
+++ lib/Travel.dart
@@ -12,116 +12,234 @@ class _M15_examState extends State<M15_exam> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Card(
-            child: Container(
-              child: Column(
-                children: [
-                  SizedBox(height: 30),
-                  Center(child: CircleAvatar(radius: 50,backgroundImage: AssetImage('asset/11.jpeg'),)),
-                  Text(
-                    'John Doe',
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    'Flutter Developer',
-                    style: TextStyle(fontSize: 14, color: Colors.grey),
-                  ),
-                  SizedBox(height: 15),
-                  Text(
-                    'Passionate about creating user-friendly and engaging digital experiences.',
-                    style: TextStyle(color: Colors.grey.shade800),
-                  ),
-                  Divider(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      SizedBox(width: 35),
-                      Icon(Icons.mail),
-                      SizedBox(width: 10),
-                      Text('john.deo@example.com'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(width: 35),
-                      Icon(Icons.phone),
-                      SizedBox(width: 10),
-                      Text('+123 456 7890'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      SizedBox(width: 60, height: 90),
-                      Card(
-                        color: Colors.blue,
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text(
-                            'Follow',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ),
-                      Card(
-                        child: TextButton(
-                          onPressed: () {},
-                          child: Text('Meassage',style: TextStyle(color: Colors.black),),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadiusGeometry.circular(10),
-                color: Colors.white,
-              ),
-              height: 400,
-            ),
-          ),
-          Row(
-            children: const [
-              Expanded(child: Divider()),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8),
-                child: Text("Interests",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 17),),
-              ),
-              Expanded(child: Divider()),
-            ],
-          ),
-          SizedBox(height: 30.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
             Card(
               child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.r),
+                  color: Colors.white,
+                ),
+                height: 400.h,
                 child: Column(
                   children: [
-                    Image.asset('asset/camra.jpg',height: 100,width: 200,)
+                    SizedBox(height: 30.h),
+                    Center(
+                      child: CircleAvatar(
+                        radius: 50.r,
+                        backgroundImage: AssetImage('asset/11.jpeg'),
+                      ),
+                    ),
+                    Text(
+                      'John Doe',
+                      style: TextStyle(
+                        fontSize: 20.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      'Flutter Developer',
+                      style: TextStyle(fontSize: 14.sp, color: Colors.grey),
+                    ),
+                    SizedBox(height: 14.h),
+                    Text(
+                      'Passionate about creating user-friendly and engaging digital experiences.',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(color: Colors.grey.shade800),
+                    ),
+                    Divider(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        SizedBox(width: 20),
+                        Icon(Icons.mail, size: 22),
+                        SizedBox(width: 10),
+                        Text(
+                          'john.deo@example.com',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(width: 20),
+                        Icon(Icons.phone, size: 22),
+                        SizedBox(width: 10),
+                        Text('+123 456 7890', style: TextStyle(fontSize: 18)),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        SizedBox(width: 45.w, height: 70.h),
+                        Card(
+                          color: Colors.blue,
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Follow',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Card(
+                          child: TextButton(
+                            onPressed: () {},
+                            child: Text(
+                              'Message',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 15,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ],
-                ),
-                height: 220.h,
-                width: 160.w,
-                decoration: BoxDecoration(
-                    color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.r)
                 ),
               ),
             ),
-            Card(
-              child: Container(
-                height: 220.h,
-                width: 160.w,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(20.r),
+            SizedBox(height: 20.h),
+            Row(
+              children: const [
+                Expanded(child: Divider()),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8),
+                  child: Text(
+                    "Interests",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                  ),
                 ),
-              ),
-            )
-          ],)
-        ],
+                Expanded(child: Divider()),
+              ],
+            ),
+            SizedBox(height: 30.h),
+            Row(
+              children: [
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Card(
+                      child: Container(
+                        height: 200.h,
+                        width: 150.w,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(20.r),
+                                bottom: Radius.circular(20.r),
+                              ),
+                              child: Image.asset(
+                                'asset/sea.jpg',
+                                height: 100.h,
+                                width: 150.w,
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Travel',
+                                    style: TextStyle(fontSize: 18.sp),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              'Exploring new place around the world',
+                              style: TextStyle(fontSize: 8.sp),
+                            ),
+                            SizedBox(height: 15.h),
+                            SizedBox(
+                              width: double.infinity.w,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.r),
+                                  ),
+                                ),
+                                child: Text('View More'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Card(
+                      child: Container(
+                        height: 200.h,
+                        width: 150.w,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10.r),
+                        ),
+                        child: Column(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.vertical(
+                                top: Radius.circular(20.r),
+                                bottom: Radius.circular(20.r),
+                              ),
+                              child: Image.asset(
+                                'asset/camera.jpg',
+                                height: 100.h,
+                                width: 150.w,
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: Column(
+                                children: [
+                                  Text(
+                                    'Photography',
+                                    style: TextStyle(fontSize: 18.sp),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Text(
+                              'Capturing moments through the lens',
+                              style: TextStyle(fontSize: 8.sp),
+                            ),
+                            SizedBox(height: 15.h),
+                            SizedBox(
+                              width: double.infinity.w,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                style: ElevatedButton.styleFrom(
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(10.r),
+                                  ),
+                                ),
+                                child: Text('View More'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
